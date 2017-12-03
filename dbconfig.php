4e1ab4bb5a50603d07193c2411646cc32c8e20a6
sanjayav <sanjayav50@yahoo.in>
2017-12-03 06:14:20 +0530

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,10 @@
+ 
+<?php
+
+ 
+define('DB_USER', "id3758224_dmaps");     // Your database user name
+define('DB_PASSWORD', "dmaps9988");			// Your database password (mention your db password here)
+define('DB_DATABASE', "id3758224_dmaps"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+ 
+?>
